5b601159c262408c4b82c0246cec8a3df044e193
lc10934 <108024247+lc10934@users.noreply.github.com>
2022-07-12 09:17:54 -0500

    Update slackbot.py

--- slackbot.py
+++ slackbot.py
@@ -6,29 +6,35 @@ import ssl
 import sys
 import certifi
 
-DATASET_DIR = '/data2/pypi'
+DATASET_DIR = '/Users/logancheng/Downloads/RESULTS' #'/data2/pypi'
 PREVIOUS_NUMBER_OF_FILES = '/home/lsc210003/numfiles.txt'
-try:
-    with open(PREVIOUS_NUMBER_OF_FILES) as f:
-        contents = f.readlines()
-except FileNotFoundError:
-    contents = 0
-    print("file not found", file=sys.stderr)
-list = os.listdir(DATASET_DIR) 
-number_files = len(list)
+def saved_previous_count(file_path: Path) -> int:
+    if not os.path.exists(file_path):
+        return 0
+    with open(file_path) as f:
+        contents = f.read()
+        return int(contents) if contents.isdecimal() else 0
+def count_files(dataset_dir: Path) -> int:
+    return len(os.listdir(dataset_dir))
+def save_file_count(file_path: Path, file_count: int):
+    with open(file_path, 'w') as f:
+        f.write(str(file_count))
+def send_slack_message(text: str):
+    env_path = Path('.') / '.env'
+    load_dotenv(dotenv_path=env_path)
 
-if not contents:
-    contents = 0
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+    client = slack.WebClient(token=os.environ['SLACK_TOKEN'], sel = ssl_context)
+    client.chat_postMessage(channel='#pypi-dataset-daily', text=text)
 
-new_files = number_files - int(contents[0])
+def main():
+    existing_files = saved_previous_count(PREVIOUS_NUMBER_OF_FILES)
+    current_files = count_files(DATASET_DIR)
+    delta = current_files - existing_files
 
-with open(PREVIOUS_NUMBER_OF_FILES, 'w') as f:
-    f.write(str(number_files))
-    
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+    message = f'Today, {delta} new files were downloaded. The PyPI dataset now has {current_files} files'
+    send_slack_message(message)
 
-ssl_context = ssl.create_default_context(cafile=certifi.where())
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'], ssl=ssl_context)
-
-client.chat_postMessage(channel='#test', text=new_files)
+    save_file_count(PREVIOUS_NUMBER_OF_FILES, current_files)
+if __name__ == '__main__':
+        main()
